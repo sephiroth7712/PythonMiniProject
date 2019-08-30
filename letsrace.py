54065d1902478f57309b5fc22c7169486b15b827
Roshan James <roshanjames7@gmail.com>
2019-08-31 02:29:02 +0530

    Improved highscore storage, added soundtracks

--- letsrace.py
+++ letsrace.py
@@ -3,8 +3,15 @@ import time
 import random
 from screeninfo import get_monitors
 import math
+import json
 
 pygame.init()
+
+SONG_FINISHED = pygame.USEREVENT + 1
+
+soundtracks=['soundtrack1.wav','soundtrack2.wav']
+pygame.mixer.music.set_endevent(SONG_FINISHED)
+crash_sound = pygame.mixer.Sound("car_crash.wav")
 
 display_width=get_monitors()[0].width
 display_height=get_monitors()[0].height
@@ -19,7 +26,6 @@ brown=(165,42,42)
 car_width=60
 
 
-
 gameDisplay=pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
 pygame.display.set_caption("Lets Race")
@@ -31,43 +37,39 @@ Car.append(pygame.image.load("Car3.png"))
 Car.append(pygame.image.load("Car4.png"))
 
 def get_high_score():
-    high_score = 0
-    try:
-        high_score_file = open("high_score.txt", "r")
-        high_score = int(high_score_file.read())
-        high_score_file.close()
+    high_scores=0
+    try: 
+        with open('high_score.json','r') as f:
+            high_scores= json.load(f)
+            f.close()
     except IOError:
         print("There is no high score yet.")
     except ValueError:
         print("I'm confused. Starting with no high score.")
-    return high_score
+    return high_scores
 
 def save_high_score(new_high_score):
     try:
-        high_score_file = open("high_score.txt", "w")
-        high_score_file.write(str(new_high_score))
-        high_score_file.close()
+        with open("high_score.json","w") as f:
+            f.write(json.dumps(new_high_score))
+            f.close()
     except IOError:
         print("Unable to save the high score.")
 
-def things_dodged(count):
+def things_dodged(count,nop,diff):
     high_score = get_high_score()
     font=pygame.font.SysFont(None,40)
     text=font.render("Score "+str(count),True,black)
-    if count > high_score:
-        save_high_score(count)
-    highScore=font.render("High Score "+str(high_score),True,black)
+    if count > high_score[str(nop)][diff]:
+        high_score[str(nop)][diff]=count
+        save_high_score(high_score)
+    highScore=font.render("High Score "+str(high_score[str(nop)][diff]),True,black)
     gameDisplay.blit(text,(20,20))
     gameDisplay.blit(highScore,(20,50))
 
 def car1(car,x,y):
     gameDisplay.blit(car,(x,y))
     return car.get_rect(x=x, y=y)
-
-def car2(x,y):
-    gameDisplay.blit(CarB,(x,y))
-    return CarB.get_rect(x=x, y=y)
-
 
 def things(thingx,thingy,thingw,thingh,color):
     color=brown
@@ -97,6 +99,8 @@ def message_display2(text):
     time.sleep(2)
 
 def crash(car):
+    pygame.mixer.Sound.play(crash_sound)
+    pygame.mixer.music.stop()
     message_display('Player '+car+'Crashed')
     
 
@@ -104,6 +108,7 @@ def crash3():
 	message_display('Collision')
 
 def game_loop(): 
+    song_idx=0
     controls=[[pygame.K_w,pygame.K_a,pygame.K_s,pygame.K_d],[pygame.K_UP,pygame.K_LEFT,pygame.K_DOWN,pygame.K_RIGHT],[pygame.K_i,pygame.K_j,pygame.K_k,pygame.K_l],[pygame.K_KP8,pygame.K_KP4,pygame.K_KP5,pygame.K_KP6]]  
     max_nob = 10
     max_players=4
@@ -147,6 +152,7 @@ def game_loop():
         car_controls[2]=controls[1]
         car_controls[3]=controls[3]
 
+    difficulty=""
     message_display2('Choose a difficulty 1.Easy 2.Medium 3.Hard')
     while(starting_speed==0):
         for event in pygame.event.get():
@@ -155,10 +161,13 @@ def game_loop():
                         quit()
             if event.type==pygame.KEYDOWN:
                 if event.key==pygame.K_1:
+                    difficulty="easy"
                     starting_speed=4
                 elif event.key==pygame.K_2:
+                    difficulty="medium"
                     starting_speed=9
                 elif event.key==pygame.K_3:
+                    difficulty="hard"
                     starting_speed=14
                 elif event.key == pygame.K_4:
                     pygame.quit()
@@ -187,6 +196,8 @@ def game_loop():
     gameExit=False
     nob=1
     keypressed=[0]*number_of_players
+    pygame.mixer.music.load(soundtracks[song_idx])
+    pygame.mixer.music.play(0)
     while not gameExit:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
@@ -221,6 +232,12 @@ def game_loop():
                         x_change[i]=0
                         y_change[i]=0
 
+            if event.type==SONG_FINISHED:
+                song_idx+=1
+                song_idx %= len(soundtracks)
+                pygame.mixer.music.load(soundtracks[song_idx])
+                pygame.mixer.music.play(0)
+
 
         for i in range(0,number_of_players):          
             if y[i]+y_change[i]>=display_height:
@@ -235,6 +252,8 @@ def game_loop():
             x[i]+=x_change[i]
         gameDisplay.fill(white)
 
+        
+
         for i in range(0,nob):
             things(thing_startx[i],thing_starty[i],thing_width[i],thing_height[i],black)
             thing_starty[i]+=thing_speed[i]
@@ -242,7 +261,7 @@ def game_loop():
         for i in range(0,number_of_players):
             cars[i]=car1(Car[i],x[i],y[i])
 
-        things_dodged(dodged)
+        things_dodged(dodged,number_of_players,difficulty)
 
         for i in range(0,len(cars)):
             for j in range(i+1,len(cars)):
@@ -257,7 +276,7 @@ def game_loop():
                 thing_starty[i]=0-thing_height[i]
                 thing_startx[i]=random.randrange(0,display_width)
                 dodged+=1
-                thing_speed[i]+=0.3
+                thing_speed[i]+=0.1
             rect  = pygame.rect.Rect(thing_startx[i],thing_starty[i],thing_width[i],thing_height[i])
             for j in range(0,number_of_players):
                 if cars[j].colliderect(rect):
